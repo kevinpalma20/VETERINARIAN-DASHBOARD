df4634110ef17f848f514d62ba20a894a6957b5e
Kevin Anderson Palma Lluén <85182515+kevinpalma20@users.noreply.github.com>
2022-06-15 11:44:46 -0500

    REFACTOR: Header with my data.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+
+import { MyDataResponse } from 'src/app/model/response/auth/MyDataResponse';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ShowAlertService } from 'src/app/services/show-alert.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styles: [],
 })
-export class HeaderComponent {
-  constructor(private auth: AuthenticationService) {}
+export class HeaderComponent implements OnInit {
+  public data: MyDataResponse = {
+    firstName: '',
+    lastName: '',
+    fullName: '',
+    email: '',
+    phone: '',
+    role: '',
+  };
+
+  constructor(
+    private auth: AuthenticationService,
+    private showToast: ShowAlertService
+  ) {}
+
+  ngOnInit(): void {
+    this.setValue();
+  }
 
   logOut() {
     this.auth.logOut();
+  }
+
+  setValue(): void {
+    this.auth.myPersonalData().subscribe(
+      (data) => (this.data = data),
+      (error: any) => this.showToast.showToast(error.error.message, false)
+    );
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,19 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from 'src/app/services/authentication.service';
+
 import { SidebarService } from 'src/app/services/sidebar.service';
+import { ShowAlertService } from 'src/app/services/show-alert.service';
+import { MyDataResponse } from 'src/app/model/response/auth/MyDataResponse';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styles: [],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  public data: MyDataResponse = {
+    firstName: '',
+    lastName: '',
+    fullName: '',
+    email: '',
+    phone: '',
+    role: '',
+  };
+
   constructor(
     public sidebarService: SidebarService,
-    private auth: AuthenticationService
+    private auth: AuthenticationService,
+    private showToast: ShowAlertService
   ) {}
+
+  ngOnInit(): void {
+    this.setValue();
+  }
 
   logOut() {
     this.auth.logOut();
+  }
+
+  setValue(): void {
+    this.auth.myPersonalData().subscribe(
+      (data) => (this.data = data),
+      (error: any) => this.showToast.showToast(error.error.message, false)
+    );
   }
 }
